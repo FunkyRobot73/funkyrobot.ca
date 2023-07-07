090841173e92c9921f1e4a20684a507fa342f6a5
Carlos <arlo252@gmail.com>
2023-07-06 23:52:05 -0400

    Added deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FunkyRobot.ca';
+  data = {
+    title: 'FunkyRobot.ca'
+  };
+
+  onLogoClick() {
+    alert("Hello Human")
+  }
+  onKeyUp(newTitle:string) {
+this.data.title = newTitle;
+  }
 }
